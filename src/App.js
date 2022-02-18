50c96d1b9e3f578bc966a5fb07c989d5e602f311
Praduman kumar <75528695+Praduman80@users.noreply.github.com>
2022-02-18 14:56:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from "react";
-// import Resturant from "./component/Basics/Resturant";cd 
-//  import UseState from "./component/Hooks/useState";
-//  import UseEffect from "./component/Hooks/useEffect";
 import UseReducer from "./component/Hooks/useReducer";
 
 const App = () => {
